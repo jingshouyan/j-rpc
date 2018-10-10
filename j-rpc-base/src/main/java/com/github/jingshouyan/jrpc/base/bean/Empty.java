eb3234e9bb5fc577fe2c8ccde084bc654350208f
Jing Shouyan <290173092@qq.com>
2018-10-10 20:33:21 +0800

    base thrift

--- j-rpc-base/src/main/java/com/github/jingshouyan/jrpc/base/bean/Empty.java
+++ j-rpc-base/src/main/java/com/github/jingshouyan/jrpc/base/bean/Empty.java
@@ -4,5 +4,5 @@ package com.github.jingshouyan.jrpc.base.bean;
  * @author jingshouyan
  * 10/10/18 5:05 PM
  */
-public class Emptry {
+public class Empty {
 }
--- j-rpc-base/src/main/java/com/github/jingshouyan/jrpc/base/bean/Rsp.java
+++ j-rpc-base/src/main/java/com/github/jingshouyan/jrpc/base/bean/Rsp.java
@@ -1,8 +1,16 @@
 package com.github.jingshouyan.jrpc.base.bean;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * @author jingshouyan
  * 10/10/18 7:26 PM
  */
+@Getter@Setter@ToString
 public class Rsp {
+    private int code;
+    private String message;
+    private String result;
 }
