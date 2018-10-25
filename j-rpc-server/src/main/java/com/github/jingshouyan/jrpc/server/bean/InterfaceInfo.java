9a87b90f5e28ddaab711ce71ec81639947befc3b
Jing Shouyan <290173092@qq.com>
2018-10-25 09:55:12 +0800

    thrift server

---
+++ j-rpc-server/src/main/java/com/github/jingshouyan/jrpc/server/bean/InterfaceInfo.java
@@ -0,0 +1,20 @@
+package com.github.jingshouyan.jrpc.server.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+/**
+ * @author jingshouyan
+ * #date 2018/10/23 11:24
+ */
+@Getter
+@Setter
+@ToString
+public class InterfaceInfo {
+    private com.github.jingshouyan.jrpc.base.bean.ServerInfo serverInfo;
+    private List<MethodInfo> methodInfos;
+    private List<CodeInfo> codeInfos;
+}
