d0082404a272ea58e666ac213895415c57be1e14
Jing Shouyan <290173092@qq.com>
2018-10-26 16:51:34 +0800

    close pool when server info removed form zk

--- j-rpc-client/src/main/java/com/github/jingshouyan/jrpc/client/RequestBuilder.java
+++ j-rpc-client/src/main/java/com/github/jingshouyan/jrpc/client/RequestBuilder.java
@@ -48,11 +48,11 @@ public class RequestBuilder {
         cfg.setMaxIdle(config.getPoolMaxIdle());
         cfg.setMaxTotal(config.getPoolMaxTotal());
         this.transportProvider = new TransportProvider(cfg);
-//        zkDiscover.addListener((event, serverInfo) -> {
-//            if(event == DiscoverEvent.REMOVE){
-//                this.transportProvider.close(serverInfo);
-//            }
-//        });
+        zkDiscover.addListener((event, serverInfo) -> {
+            if(event == DiscoverEvent.REMOVE){
+                this.transportProvider.close(serverInfo);
+            }
+        });
     }
 
     public Request newRequest(){
