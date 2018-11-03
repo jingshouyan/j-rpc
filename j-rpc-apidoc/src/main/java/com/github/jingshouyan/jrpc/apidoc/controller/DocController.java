c45e53222c139c70c84804350b751bbe5e9546ff
Jing Shouyan <290173092@qq.com>
2018-11-03 23:57:27 +0800

    well down! ttl trace

--- j-rpc-apidoc/src/main/java/com/github/jingshouyan/jrpc/apidoc/controller/DocController.java
+++ j-rpc-apidoc/src/main/java/com/github/jingshouyan/jrpc/apidoc/controller/DocController.java
@@ -1,30 +1,44 @@
 package com.github.jingshouyan.jrpc.apidoc.controller;
 
+import brave.Tracing;
+import com.alibaba.ttl.TransmittableThreadLocal;
 import com.github.jingshouyan.jrpc.base.bean.Rsp;
 import com.github.jingshouyan.jrpc.base.bean.ServerInfo;
 import com.github.jingshouyan.jrpc.base.util.rsp.RspUtil;
 import com.github.jingshouyan.jrpc.client.JrpcClient;
 import com.github.jingshouyan.jrpc.client.Request;
 import com.google.common.collect.Lists;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.IntStream;
 
 /**
  * @author jingshouyan
  * #date 2018/10/26 14:58
  */
 @Controller
+@Slf4j
 public class DocController {
 
-    ExecutorService exec = Executors.newFixedThreadPool(10);
+
+
+    ExecutorService exec;
+
+    public DocController(Tracing tracing){
+        ExecutorService exec = Executors.newFixedThreadPool(10);
+        this.exec = tracing.currentTraceContext().executorService(exec);
+        this.exec = exec;
+    }
 
     @Resource
     private JrpcClient jrpcClient;
@@ -32,6 +46,7 @@ public class DocController {
     @RequestMapping("servers")
     @ResponseBody
     public Rsp serverMap(){
+
         Map<String,List<ServerInfo>> map = jrpcClient.serverMap();
         List<ServerInfo> serverInfos = Lists.newArrayList();
         for(List<ServerInfo> l: map.values()){
@@ -41,20 +56,41 @@ public class DocController {
         }
         return RspUtil.success(serverInfos);
     }
-
+    private static final TransmittableThreadLocal<String> TL_TEST = new TransmittableThreadLocal<>();
     @RequestMapping("server/{server}")
     @ResponseBody
     public String serverInfo(@PathVariable String server){
+
         String str = Request.newInstance().setClient(jrpcClient)
                 .setServer(server)
                 .setMethod("getServerInfo")
                 .send().json();
-        exec.execute(()->{
-            Request.newInstance().setClient(jrpcClient)
-                    .setServer(server)
-                    .setMethod("getServerInfo")
-                    .send().json();
+
+        TL_TEST.set("abc"+System.currentTimeMillis());
+        log.info("M:TL_TEST:{}",TL_TEST.get());
+//        for (int i = 0; i < 10; i++) {
+//            exec.execute(new Runnable() {
+//                @Override
+//                public void run() {
+//                    log.info("M:TL_TEST:{}",TL_TEST.get());
+//                }
+//            });
+//        }
+        IntStream.range(0,10)
+                .parallel()
+                .forEach(i ->{
+//            exec.execute(()->{
+                log.info("R:TL_TEST:{}",TL_TEST.get());
+                Request.newInstance().setClient(jrpcClient)
+                        .setServer(server)
+                        .setMethod("getServerInfo")
+                        .send().json();
+//            });
         });
+
+        List<String> list = new ArrayList<>();
+        list.parallelStream().mapToInt(String::length).sum();
+
         return str;
     }
 
