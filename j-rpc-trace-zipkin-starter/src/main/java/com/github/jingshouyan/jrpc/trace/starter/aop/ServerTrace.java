c0fc2d3c0ab09ad638c979872af8151ee8d5f6e2
Jing Shouyan <290173092@qq.com>
2019-01-12 12:47:26 +0800

    order

--- j-rpc-trace-zipkin-starter/src/main/java/com/github/jingshouyan/jrpc/trace/starter/aop/ServerTrace.java
+++ j-rpc-trace-zipkin-starter/src/main/java/com/github/jingshouyan/jrpc/trace/starter/aop/ServerTrace.java
@@ -16,12 +16,15 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 /**
  * @author jingshouyan
  * #date 2018/11/2 20:00
  */
 @Aspect
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ServerTrace implements TraceConstant {
 
     private Tracer tracer;
