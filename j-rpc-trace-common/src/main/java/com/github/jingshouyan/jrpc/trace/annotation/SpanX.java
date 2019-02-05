99883161641b5936bff306eec2b3a212fd9aeeec
Jingshouyan <290173092@qq.com>
2019-02-05 17:12:55 +0800

    trace

--- j-rpc-trace-common/src/main/java/com/github/jingshouyan/jrpc/trace/annotation/SpanX.java
+++ j-rpc-trace-common/src/main/java/com/github/jingshouyan/jrpc/trace/annotation/SpanX.java
@@ -1,4 +1,11 @@
 package com.github.jingshouyan.jrpc.trace.annotation;
 
+import java.lang.annotation.*;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface SpanX {
+    String spanName() default "";
+    boolean showData() default false;
 }
