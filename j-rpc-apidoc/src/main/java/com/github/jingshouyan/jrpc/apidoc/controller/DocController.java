820b3a9a0964ae7a1eb63dd12a3505da02c7af89
Jingshouyan <290173092@qq.com>
2019-04-27 12:54:31 +0800

    code(doc controller): codes

--- j-rpc-apidoc/src/main/java/com/github/jingshouyan/jrpc/apidoc/controller/DocController.java
+++ j-rpc-apidoc/src/main/java/com/github/jingshouyan/jrpc/apidoc/controller/DocController.java
@@ -1,46 +1,40 @@
 package com.github.jingshouyan.jrpc.apidoc.controller;
 
-import brave.Tracing;
+import com.github.jingshouyan.jrpc.base.bean.CodeInfo;
+import com.github.jingshouyan.jrpc.base.bean.InterfaceInfo;
+import com.github.jingshouyan.jrpc.base.bean.Rsp;
 import com.github.jingshouyan.jrpc.base.bean.ServerInfo;
 import com.github.jingshouyan.jrpc.base.util.rsp.RspUtil;
 import com.github.jingshouyan.jrpc.client.JrpcClient;
 import com.github.jingshouyan.jrpc.client.Request;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @author jingshouyan
  * #date 2018/10/26 14:58
  */
-@Controller
+@RestController
 @Slf4j
 public class DocController {
 
-
-
-    ExecutorService exec;
-
-    public DocController(Tracing tracing){
-        ExecutorService exec = Executors.newFixedThreadPool(10);
-        this.exec = tracing.currentTraceContext().executorService(exec);
-    }
 
     @Resource
     private JrpcClient jrpcClient;
 
     @RequestMapping("servers")
-    @ResponseBody
-    public String serverMap(){
+    public String servers(){
 
         Map<String,List<ServerInfo>> map = jrpcClient.serverMap();
         List<ServerInfo> serverInfos = Lists.newArrayList();
@@ -51,9 +45,42 @@ public class DocController {
         }
         return RspUtil.success(serverInfos).json();
     }
+    @RequestMapping("codes")
+    public String codes() {
+        Map<String,List<ServerInfo>> map = jrpcClient.serverMap();
+        List<String> servers = map.values().stream().filter(Objects::nonNull)
+                .filter(list -> !list.isEmpty())
+                .map(list -> list.get(0).getName())
+                .collect(Collectors.toList());
+        List<CodeInfo> codes = Lists.newArrayList();
+        for (String server: servers) {
+            Rsp rsp = Request.newInstance().setClient(jrpcClient)
+                    .setServer(server)
+                    .setMethod("getServerInfo")
+                    .send();
+            if(rsp.success()){
+                InterfaceInfo info = rsp.get(InterfaceInfo.class);
+                List<CodeInfo> codeInfos = info.getCodeInfos();
+                codeInfos.forEach(c -> c.setWhoUse(server));
+                codes.addAll(codeInfos);
+            }
+        }
+        Map<String,CodeInfo> cmap = Maps.newHashMap();
+        for (CodeInfo code: codes) {
+            String key = code.getCode()+":"+code.getMessage();
+            if(cmap.containsKey(key)){
+                CodeInfo code2 = cmap.get(key);
+                code2.setWhoUse(code2.getWhoUse()+ ","+ code.getWhoUse());
+            }else {
+                cmap.put(key,code);
+            }
+        }
+        codes = Lists.newArrayList(cmap.values());
+        codes.sort(Comparator.comparingInt(CodeInfo::getCode));
+        return RspUtil.success(codes).json();
+    }
 
     @RequestMapping("server/{server}")
-    @ResponseBody
     public String serverInfo(@PathVariable String server){
 
         String str = Request.newInstance().setClient(jrpcClient)
