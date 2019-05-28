7d7db12c977920e43e7ddd2a04219adf146819f1
靖守彦 <290173092@qq.com>
2019-05-28 19:58:48 +0800

    feat(json): java8 数据类型json支持

--- j-rpc-base/src/main/java/com/github/jingshouyan/jrpc/base/util/json/JsonUtil.java
+++ j-rpc-base/src/main/java/com/github/jingshouyan/jrpc/base/util/json/JsonUtil.java
@@ -3,6 +3,9 @@ package com.github.jingshouyan.jrpc.base.util.json;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.type.TypeBindings;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import lombok.SneakyThrows;
 import org.apache.thrift.TBase;
 import org.apache.thrift.TSerializer;
@@ -23,6 +26,9 @@ public class JsonUtil {
 
     private static final ObjectMapper OBJECT_MAPPER = Optional.<ObjectMapper>empty().orElseGet(()->{
         ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new ParameterNamesModule())
+                .registerModule(new Jdk8Module())
+                .registerModule(new JavaTimeModule());
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
         objectMapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE,false);
         objectMapper.configure(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE,false);
