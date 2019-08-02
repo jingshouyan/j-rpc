e869129c9f7faa8a26e369e1318f2e59cab6b50c
靖守彦 <290173092@qq.com>
2019-08-02 15:21:03 +0800

    test(trace): trace gather test

--- j-rpc-test/j-rpc-test-server/src/main/java/com/jing/test/TestApp.java
+++ j-rpc-test/j-rpc-test-server/src/main/java/com/jing/test/TestApp.java
@@ -1,8 +1,14 @@
 package com.jing.test;
 
+import com.github.jingshouyan.jrpc.starter.trace.TraceProperties;
+import com.github.jingshouyan.jrpc.trace.interceptor.ServerTrace;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * @author jingshouyan
@@ -10,8 +16,19 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 @SpringBootApplication
 @EnableScheduling
+@Slf4j
 public class TestApp {
     public static void main(String[] args) {
         SpringApplication.run(TestApp.class, args);
+    }
+
+    @Autowired
+    private ApplicationContext ctx;
+    @Scheduled(fixedRate = 5000)
+    public void job(){
+        ServerTrace serverTrace = ctx.getBean(ServerTrace.class);
+        System.out.println(serverTrace.getGather());
+        TraceProperties traceProperties = ctx.getBean(TraceProperties.class);
+        System.out.println(traceProperties);
     }
 }
