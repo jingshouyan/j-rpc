9efb97ce6345544652f650efd80f8de1e7e04101
JingShouyan <jingshouyan@gmail.com>
2021-09-14 17:56:31 +0800

    fix(forward): 修复forward 版本缺失

--- j-rpc-starter/j-rpc-starter-forward/src/main/java/com/github/jingshouyan/starter/forward/ForwardAutoConfiguration.java
+++ j-rpc-starter/j-rpc-starter-forward/src/main/java/com/github/jingshouyan/starter/forward/ForwardAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.github.jingshouyan.starter.forward;
 
 import com.github.jingshouyan.jrpc.base.action.ActionInterceptorHolder;
 import com.github.jingshouyan.jrpc.client.JrpcClient;
+import com.github.jingshouyan.jrpc.starter.server.ServerProperties;
 import com.github.jingshouyan.starter.forward.aop.JrpcForward;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -24,10 +25,13 @@ public class ForwardAutoConfiguration {
     @Resource
     private JrpcClient client;
 
+    @Resource
+    private ServerProperties serverProperties;
+
     @Bean
     @ConditionalOnMissingBean(JrpcForward.class)
     JrpcForward jrpcForward() {
-        JrpcForward forward = new JrpcForward(client, properties);
+        JrpcForward forward = new JrpcForward(client, properties, serverProperties.getVersion());
         ActionInterceptorHolder.addServerInterceptor(forward);
         return forward;
     }
--- j-rpc-starter/j-rpc-starter-forward/src/main/java/com/github/jingshouyan/starter/forward/aop/JrpcForward.java
+++ j-rpc-starter/j-rpc-starter-forward/src/main/java/com/github/jingshouyan/starter/forward/aop/JrpcForward.java
@@ -8,33 +8,65 @@ import com.github.jingshouyan.jrpc.base.bean.Token;
 import com.github.jingshouyan.jrpc.client.JrpcClient;
 import com.github.jingshouyan.jrpc.client.Request;
 import com.github.jingshouyan.starter.forward.ForwardProperties;
+import com.google.common.collect.Maps;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
+
+import java.util.Map;
 
 /**
  * @author jingshouyan
  * #date 2019/1/12 11:25
  */
 @Slf4j
-@AllArgsConstructor
 public class JrpcForward implements ActionInterceptor {
 
-    private JrpcClient client;
-    private ForwardProperties properties;
+    private static final char POINT = '.';
+    private static final char AT = '@';
+
+    private final JrpcClient client;
+
+    private final Map<String, Forward> forwardMap;
+
+    public JrpcForward(JrpcClient client, ForwardProperties properties, String defaultVersion) {
+        this.client = client;
+        forwardMap = Maps.newHashMapWithExpectedSize(properties.getMethods().size());
+        for (Map.Entry<String, String> entry : properties.getMethods().entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            int idx = value.indexOf(POINT);
+            if (idx > 0) {
+                String service = value.substring(0, idx);
+                String method = value.substring(idx);
+                String version = defaultVersion;
+                idx = service.indexOf(AT);
+                if (idx > 0) {
+                    version = service.substring(idx);
+                    service = service.substring(0, idx);
+                }
+                Forward f = new Forward(service, version, method);
+                forwardMap.put(key, f);
+                log.info("{} forward to {}", key, f);
+            } else {
+                log.warn("{} ==> {}, wrong format",key,value);
+            }
+        }
+    }
 
     @Override
     public Mono<Rsp> around(Token token, Req req, ActionHandler handler) {
-        if (properties.getMethods().containsKey(req.getMethod())) {
-            String str = properties.getMethods().get(req.getMethod());
-            String[] strings = str.split("\\.");
-            String server = strings[0];
-            String method = strings[1];
-            log.debug("{} forward to {}.{}", req.getMethod(), server, method);
+        Forward f = forwardMap.get(req.getMethod());
+        if (f != null) {
+            log.debug("{} forward to {}", req.getMethod(), f);
             return Request.newInstance()
                     .setClient(client)
-                    .setServer(server)
-                    .setMethod(method)
+                    .setServer(f.service)
+                    .setVersion(f.version)
+                    .setMethod(f.method)
                     .setToken(token)
                     .setParamJson(req.getParam())
                     .setOneway(req.isOneway())
@@ -46,5 +78,20 @@ public class JrpcForward implements ActionInterceptor {
     @Override
     public int order() {
         return Integer.MIN_VALUE + 1;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static class Forward {
+        private String service;
+        private String version;
+        private String method;
+
+        @Override
+        public String toString() {
+            return service + AT + version + POINT + method;
+        }
     }
 }
